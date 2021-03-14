4bb2378f30c71ea4f8e1bd5270373da39b2f0924
Patryk Plaza <atrykp0@gmail.com>
2021-03-14 14:48:07 +0100

    make it better

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const { json } = require("express");
 const express = require("express");
 require("dotenv").config();
 const app = express();
-app.listen(5000, () => {
-  console.log("listen on port 5000");
-});
+const port = process.env.PORT || 5000;
+app.listen(port);
 app.use(express.json({ limit: "1mb" }));
 
 const LanguageTranslatorV3 = require("ibm-watson/language-translator/v3");
@@ -13,19 +11,21 @@ const { IamAuthenticator } = require("ibm-watson/auth");
 const languageTranslator = new LanguageTranslatorV3({
   version: "2018-05-01",
   authenticator: new IamAuthenticator({
-    apikey: process.env.API_KEY,
+    apikey: `${process.env.API_KEY}`,
   }),
-  serviceUrl: process.env.API_URL,
-  disableSslVerification: true,
+
+  serviceUrl: `${process.env.API_URL}`,
 });
 
 app.get("/translate/:word", async (request, response) => {
   const translateThis = request.params.word;
+
   const translateParams = {
     text: translateThis,
     source: "en",
     target: "pl",
   };
+
   const translateResponse = await languageTranslator.translate(translateParams);
 
   response.json(translateResponse);
