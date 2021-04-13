b882dc2cb8de867c8d1e898537c1626968d3e6e7
Patryk Plaza <atrykp0@gmail.com>
2021-04-13 06:21:20 +0200

    add new endpoint to listen translation

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fs = require("fs");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
@@ -7,7 +8,7 @@ app.listen(port);
 app.use(express.json({ limit: "1mb" }));
 
 const corsOptions = {
-  origin: "https://pensive-lalande-2ecd72.netlify.app",
+  origin: ["https://pensive-lalande-2ecd72.netlify.app"],
 };
 
 const LanguageTranslatorV3 = require("ibm-watson/language-translator/v3");
@@ -20,6 +21,15 @@ const languageTranslator = new LanguageTranslatorV3({
   }),
 
   serviceUrl: `${process.env.API_URL}`,
+});
+
+const TextToSpeechV1 = require("ibm-watson/text-to-speech/v1");
+
+const textToSpeech = new TextToSpeechV1({
+  authenticator: new IamAuthenticator({
+    apikey: `${process.env.API_LISTEN_KEY}`,
+  }),
+  serviceUrl: `${process.env.API_LISTEN_URL}`,
 });
 
 app.get("/", cors(corsOptions), async (request, response) => {
@@ -44,5 +54,33 @@ app.get(
     );
 
     response.json(translateResponse);
+  }
+);
+
+app.get(
+  "/translate/listen/:word/:from/:to",
+  cors(corsOptions),
+  async (request, response) => {
+    const translateThis = request.params.word;
+
+    const synthesizeParams = {
+      text: "Hello world",
+      accept: "audio/wav",
+      voice: "en-US_AllisonV3Voice",
+    };
+
+    textToSpeech
+      .synthesize(synthesizeParams)
+      .then((response) => {
+        // only necessary for wav formats,
+        // otherwise `response.result` can be directly piped to a file
+        return textToSpeech.repairWavHeaderStream(response.result);
+      })
+      .then((buffer) => {
+        fs.writeFileSync("hello_world.wav", buffer);
+      })
+      .catch((err) => {
+        console.log("error:", err);
+      });
   }
 );
